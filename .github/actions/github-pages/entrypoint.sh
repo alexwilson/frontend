1b6b42252f64d22f3ad8aa15c978ef2ac5097b34
Alex Wilson <alex@alexwilson.tech>
2019-09-07 16:29:30 +0300

    Update action's ref to be gh-pages too.

--- .github/actions/github-pages/entrypoint.sh
+++ .github/actions/github-pages/entrypoint.sh
@@ -8,13 +8,14 @@ REPOSITORY="https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITOR
 
 git init
 git remote add origin $REPOSITORY
+git checkout -b gh-pages
 git fetch origin gh-pages
 git reset --soft origin/gh-pages
 git config user.name "${GITHUB_ACTOR}"
 git config user.email "${GITHUB_ACTOR}@users.noreply.github.com"
 git add .
 git commit -m "Deploying ${GITHUB_SHA} to Github Pages"
-git push --force origin gh-pages
+git push --force origin gh-pages:gh-pages
 rm -fr .git
 cd $GITHUB_WORKSPACE
 
