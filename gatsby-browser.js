41305d00c67bb186061b3805eced5a620558438f
Alex Wilson <a@ax.gy>
2020-01-19 13:45:17 +0200

    Use a different Prism theme for readability

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,5 +5,5 @@
  */
 
 // You can delete this file if you're not using it
-require("prismjs/themes/prism-solarizedlight.css")
+require("prismjs/themes/prism-okaidia.css")
 require("prismjs/plugins/command-line/prism-command-line.css")
