4be6c0dce7eb0aba039bd5f660290c5be92a8f52
Alex Wilson <a@ax.gy>
2020-08-17 01:09:17 +0300

    Move content into Bucklespring

--- packages/content/index.js
+++ packages/content/index.js
@@ -3,5 +3,6 @@ const path = require('path')
 module.exports = {
     pictures: path.join(__dirname, '/pictures/'),
     posts: path.join(__dirname, '/posts/'),
-    talks: path.join(__dirname, '/talks/')
+    talks: path.join(__dirname, '/talks/'),
+    bucklespring: path.join(__dirname, '/bucklespring/')
 }
