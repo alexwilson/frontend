c86ea280fdeec0c0e61b2956bf77befb0445767b
Alex Wilson <a@ax.gy>
2021-03-27 21:16:19 +0000

    feat: Extract images from Gatsby (fixes #1555)

---
+++ packages/pictures/src/index.js
@@ -0,0 +1,9 @@
+addEventListener('fetch', function(event) {
+    event.respondWith(handleRequest(event.request))
+});
+
+async function handleRequest(request) {
+    const {pathname} = new URL(request.url)
+    const url = new URL(pathname.replace(/^\/pictures/, ''), "http://alex-images.s3-website.eu-west-1.amazonaws.com");
+    return await fetch(url, { "cf": { "cacheEverything": true } });
+}
