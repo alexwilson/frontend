bca7c8b6f2d13d9495dca3db68680318fe3db54e
Alex Wilson <a@ax.gy>
2021-03-30 23:59:32 +0100

    feat: Point to new CloudFront origin

--- packages/pictures/src/index.js
+++ packages/pictures/src/index.js
@@ -2,8 +2,9 @@ addEventListener('fetch', function(event) {
     event.respondWith(handleRequest(event.request))
 });
 
+const ORIGIN = "https://d2yriqampkvcut.cloudfront.net";
 async function handleRequest(request) {
     const {pathname} = new URL(request.url)
-    const url = new URL(pathname.replace(/^\/pictures/, ''), "http://alex-images.s3-website.eu-west-1.amazonaws.com");
+    const url = new URL(pathname.replace(/^\/pictures/, ''), ORIGIN);
     return await fetch(url, { "cf": { "cacheEverything": true, ttl: 14*24*60*60 } });
 }
