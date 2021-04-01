995b04301a5a0d11711f1000c1dce47a40d9fe11
Alex Wilson <a@ax.gy>
2021-04-01 20:28:19 +0100

    chore: Move pictures to media.alexwilson.tech

--- packages/pictures/src/index.js
+++ packages/pictures/src/index.js
@@ -2,7 +2,7 @@ addEventListener('fetch', function(event) {
     event.respondWith(handleRequest(event.request))
 });
 
-const ORIGIN = "https://d2yriqampkvcut.cloudfront.net";
+const ORIGIN = "https://media.alexwilson.tech";
 async function handleRequest(request) {
     const {pathname} = new URL(request.url)
     const url = new URL(pathname.replace(/^\/pictures/, ''), ORIGIN);
