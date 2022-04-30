3b2a6b0d8c4f253a3a887286c3bd3c66ea7ea3c1
Alex Wilson <alex@alexwilson.tech>
2022-04-30 23:26:55 +0100

    chore(github-pages): Log working directory & actor

--- .github/actions/github-pages/entrypoint.sh
+++ .github/actions/github-pages/entrypoint.sh
@@ -6,16 +6,17 @@ cd $*
 echo "Deploying ${GITHUB_SHA} to GitHub Pages"
 REPOSITORY="https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
 
+echo "Initialising in {$PWD} as {$GITHUB_ACTOR}"
 git config --global init.defaultBranch main
 git config --global --add safe.directory "{$PWD}"
+git config --global user.name "${GITHUB_ACTOR}"
+git config --global user.email "${GITHUB_ACTOR}@users.noreply.github.com"
 
 git init
 git remote add origin $REPOSITORY
 git checkout -b gh-pages
 git fetch origin gh-pages
 git reset --soft origin/gh-pages
-git config user.name "${GITHUB_ACTOR}"
-git config user.email "${GITHUB_ACTOR}@users.noreply.github.com"
 git add .
 git commit -m "Deploying ${GITHUB_SHA} to Github Pages"
 git push --force origin gh-pages:gh-pages
