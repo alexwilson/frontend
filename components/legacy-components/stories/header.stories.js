560879ecbde77866718bc3045242c09a46a60fa4
Alex Wilson <alex@alexwilson.tech>
2023-07-09 22:51:51 +0100

    feat(sb): Add base stories for legacy components

---
+++ components/legacy-components/stories/header.stories.js
@@ -0,0 +1,12 @@
+import { Header } from '../src/header';
+
+export default {
+  title: 'Legacy/Header',
+  component: Header,
+  tags: ['autodocs'],
+  parameters: {
+    layout: 'fullscreen',
+  },
+};
+
+export const Default = {};
