523adac1c8c6422a1ea8bd415b143db85db41f45
Alex Wilson <alex@alexwilson.tech>
2024-06-05 23:20:39 +0100

    feat(cms): Swap Netlify dependencies for Decap

--- services/cms/client/main.js
+++ services/cms/client/main.js
@@ -1,25 +1,24 @@
-import CMS from 'netlify-cms-app';
-import config from './config.yml';
+import CMS from "decap-cms-app";
+import config from "./config.yml";
 
-import {Uuid} from "./widgets/uuid.jsx";
+import { Uuid } from "./widgets/uuid.jsx";
 // import {YouTube} from "./widgets/editor/youtube.jsx";
 
-
 export default function init() {
-    const useTestBackend = Boolean(process.env.CMS_BACKEND === "test")
+  const useTestBackend = Boolean(process.env.CMS_BACKEND === "test");
 
-    // Remove constants.
-    delete config.__constants;
+  // Remove constants.
+  delete config.__constants;
 
-    if (useTestBackend) {
-        config.backend = {
-            name: 'test-repo'
-        }
-    }
+  if (useTestBackend) {
+    config.backend = {
+      name: "test-repo",
+    };
+  }
 
-    CMS.init({ config });
-    CMS.registerWidget('uuid', Uuid)
-    //CMS.registerEditorComponent(YouTube)
+  CMS.init({ config });
+  CMS.registerWidget("uuid", Uuid);
+  //CMS.registerEditorComponent(YouTube)
 }
 
-document.addEventListener('DOMContentLoaded', init)
+document.addEventListener("DOMContentLoaded", init);
