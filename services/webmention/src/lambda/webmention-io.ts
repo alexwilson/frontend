6ac247b627703494424ca1189cd7c6a73cfbe423
Alex Wilson <alex@alexwilson.tech>
2024-07-11 00:14:50 +0100

    feat(webmentions): Add a secret to webhook endpoint

--- services/webmention/src/lambda/webmention-io.ts
+++ services/webmention/src/lambda/webmention-io.ts
@@ -4,11 +4,20 @@ import * as AWS from "aws-sdk";
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
 const NIL_UUID = "00000000-0000-0000-0000-000000000000";
 const tableName = process.env.DYNAMODB_TABLE || "";
+const webhookToken = process.env.WEBMENTION_IO_WEBHOOK_TOKEN || undefined;
 
 export const handler: APIGatewayProxyHandler = async (
   event: APIGatewayProxyEvent,
 ) => {
   const body = JSON.parse(event.body || "");
+
+  if (webhookToken && body.secret !== webhookToken) {
+    return {
+      statusCode: 401,
+      body: JSON.stringify({ message: "Unauthorized" }),
+    };
+  }
+
   const webmentionData = body.post;
   const webmentionId = String(body.post["wm-id"]);
   const contentId = extractArticleIdFromUrl(body.target) || NIL_UUID;
