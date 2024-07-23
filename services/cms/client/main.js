19eb5dd8c7d32ea2f7b117d47ec7f2eeca1fccc9
Alex Wilson <alex@alexwilson.tech>
2024-07-23 09:15:13 +0100

    feat: Migrate to Decap CMS

--- services/cms/client/main.js
+++ services/cms/client/main.js
@@ -1,25 +1,28 @@
-import CMS from 'netlify-cms-app';
-import config from './config.yml';
+import CMS from "decap-cms-app";
+import config from "./config.yml";
 
-import {Uuid} from "./widgets/uuid.jsx";
+import { Uuid } from "./widgets/uuid.jsx";
 // import {YouTube} from "./widgets/editor/youtube.jsx";
-
+//
+import { ArticlePreview, ArticlePreviewStyles } from "./preview/article.jsx";
 
 export default function init() {
-    const useTestBackend = Boolean(process.env.CMS_BACKEND === "test")
+  const useTestBackend = Boolean(process.env.CMS_BACKEND === "test");
 
-    // Remove constants.
-    delete config.__constants;
+  // Remove constants.
+  delete config.__constants;
 
-    if (useTestBackend) {
-        config.backend = {
-            name: 'test-repo'
-        }
-    }
+  if (useTestBackend) {
+    config.backend = {
+      name: "test-repo",
+    };
+  }
 
-    CMS.init({ config });
-    CMS.registerWidget('uuid', Uuid)
-    //CMS.registerEditorComponent(YouTube)
+  CMS.init({ config });
+  CMS.registerWidget("uuid", Uuid);
+  //CMS.registerEditorComponent(YouTube)
+  CMS.registerPreviewTemplate("blog", ArticlePreview);
+  CMS.registerPreviewStyle(ArticlePreviewStyles.toString(), { raw: true });
 }
 
-document.addEventListener('DOMContentLoaded', init)
+document.addEventListener("DOMContentLoaded", init);
