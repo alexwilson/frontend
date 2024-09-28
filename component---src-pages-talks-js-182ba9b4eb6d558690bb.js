9b86b7f5019a974e21804c0052553fb5d6bb416c
alexwilson <alexwilson@users.noreply.github.com>
2024-09-28 16:29:11 +0000

    Deploying 80654ae903072443c4a1c78d00858dd9130f3831 to Github Pages

--- component---src-pages-talks-js-182ba9b4eb6d558690bb.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunk_alexwilson_personal_website=self.webpackChunk_alexwilson_personal_website||[]).push([[302],{6522:function(e,a,l){l.r(a);var t=l(2155),n=l(3789),s=l(6335);a.default=e=>{let{data:a,location:l}=e;return t.createElement(n.A,{location:l},t.createElement("div",{className:"alex-stream"},t.createElement("h1",null,"Talks"),a.talks.edges.map((e=>{let{node:a}=e;return t.createElement(s.A,{key:a.id,article:a})}))))}}}]);
-//# sourceMappingURL=component---src-pages-talks-js-182ba9b4eb6d558690bb.js.map
