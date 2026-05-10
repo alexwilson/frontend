74688771d25bb20141f8416589e9be371aad12bf
Alex Wilson <alex@alexwilson.tech>
2026-05-10 15:52:14 +0900

    feat: Major revision of compact header

--- components/legacy-components/src/footer/footer.js
+++ components/legacy-components/src/footer/footer.js
@@ -1,12 +1,25 @@
 import React from 'react'
 
+const socialLinks = [
+  { url: 'https://bsky.app/profile/alexwilson.bsky.social', icon: '/svg/bluesky.svg', title: 'Bluesky' },
+  { url: 'https://www.linkedin.com/in/alex-/', icon: '/svg/linkedin.svg', title: 'LinkedIn' },
+  { url: 'https://www.instagram.com/alx.946', icon: '/svg/instagram.svg', title: 'Instagram' },
+  { url: 'https://mastodon.social/@alexwilson', icon: '/svg/mastodon.svg', title: 'Mastodon' },
+  { url: 'https://github.com/alexwilson', icon: '/svg/github.svg', title: 'GitHub' },
+]
+
 const Footer = () => (
   <footer className="footer">
     <div className="container align-center">
       <span className="text-muted">&copy; Alex Wilson {new Date().getFullYear()}</span>
+      <span className="footer__separator"> &bull; </span>
+      {socialLinks.map(({ url, icon, title }) => (
+        <a key={title} href={url} rel="me" className="footer__social">
+          <img src={icon} alt={title} className="footer__social-link" />
+        </a>
+      ))}
     </div>
   </footer>
 )
 
 export default Footer
-
