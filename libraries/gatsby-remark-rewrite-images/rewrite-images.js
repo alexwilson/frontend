14bd97ac81eba22b9d24b033cbc6287f72b5bf58
Alex Wilson <alex@alexwilson.tech>
2026-05-24 17:00:49 +0100

    fix(gatsby-remark-rewrite-images): handle null or undefined

--- libraries/gatsby-remark-rewrite-images/rewrite-images.js
+++ libraries/gatsby-remark-rewrite-images/rewrite-images.js
@@ -1,5 +1,5 @@
 const mediaDomain = 'https://media.alexwilson.tech/'
-const rewriteImage = (src) => src
+const rewriteImage = (src) => !src ? src : src
     .replace(/^https?\:\/\/alexwilson.tech\//, '/')
     .replace(/^\/pictures\//, mediaDomain)
 
