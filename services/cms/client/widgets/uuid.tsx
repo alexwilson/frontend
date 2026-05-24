b5ae2a0e80c3de9b8bf13cd8505cbeb35f10eb5e
Alex Wilson <alex@alexwilson.tech>
2026-05-24 15:18:54 +0900

    chore(cms): replace uuid dependency with webcrypto api

--- services/cms/client/widgets/uuid.tsx
+++ services/cms/client/widgets/uuid.tsx
@@ -1,30 +1,16 @@
-import { v4 } from 'uuid';
-import React, { Component } from 'react';
+import { useEffect } from 'react';
 import type { CmsWidgetControlProps } from 'decap-cms-core';
 
-export class Uuid extends Component<CmsWidgetControlProps<string>> {
-    constructor(props: CmsWidgetControlProps<string>) {
-        super(props)
-        if (props.value) return
-        this.generateUuid()
-    }
+export function Uuid({ forID, classNameWrapper, value, onChange }: CmsWidgetControlProps<string>) {
+    // biome-ignore lint/correctness/useExhaustiveDependencies: intentionally mount-only; re-running on value/onChange changes would regenerate the UUID
+    useEffect(() => {
+        if (!value) onChange(crypto.randomUUID())
+    }, [])
 
-    componentDidUpdate() {
-        if (this.props.value) return
-        this.generateUuid()
-    }
-
-    generateUuid() {
-        this.props.onChange(v4())
-    }
-
-    render() {
-        const { forID, classNameWrapper, value } = this.props;
-        return (
-            <div className={classNameWrapper}>
-                <input type="hidden" id={forID} value={value} />
-                <code>{value}</code>
-            </div>
-        );
-    }
+    return (
+        <div className={classNameWrapper}>
+            <input type="hidden" id={forID} value={value} />
+            <code>{value}</code>
+        </div>
+    );
 }
