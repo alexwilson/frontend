0b49878dde31a8638c8b410689a775b0e72a5492
Alex Wilson <alex@alexwilson.tech>
2026-05-29 00:02:57 +0900

    feat: github pages but without the docker

--- .github/actions/github-pages/entrypoint.sh
+++ .github/actions/github-pages/entrypoint.sh
@@ -1,26 +1,22 @@
 #!/bin/sh
 set -e
 
-cd $*
+cd "$PUBLISH_DIR"
 
 echo "Deploying ${GITHUB_SHA} to GitHub Pages"
-REPOSITORY="https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
-
 echo "Initialising in ${PWD} as ${GITHUB_ACTOR}"
-git config --global init.defaultBranch main
-git config --global --add safe.directory "${PWD}"
-git config --global user.name "${GITHUB_ACTOR}"
-git config --global user.email "${GITHUB_ACTOR}@users.noreply.github.com"
 
 git init
-git remote add origin $REPOSITORY
+git config user.name "${GITHUB_ACTOR}"
+git config user.email "${GITHUB_ACTOR}@users.noreply.github.com"
+git remote add origin "https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
+
 git checkout -b gh-pages
 git fetch origin gh-pages
 git reset --soft origin/gh-pages
 git add .
-git commit -m "Deploying ${GITHUB_SHA} to Github Pages"
+git commit -m "Deploying ${GITHUB_SHA} to GitHub Pages"
 git push --force origin gh-pages:gh-pages
-rm -fr .git
-cd $GITHUB_WORKSPACE
 
+rm -fr .git
 echo "Successfully deployed."
