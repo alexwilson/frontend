8b72280c6da98d18629dbdc944d2cfd856c65257
Alex Wilson <alex@alexwilson.tech>
2026-06-01 16:53:51 +0100

    ci: silence default branch warning
    # Why?
    Git throws a small error complaining about the default branch name
    
    # What?
    Silence it

--- .github/actions/github-pages/entrypoint.sh
+++ .github/actions/github-pages/entrypoint.sh
@@ -7,6 +7,7 @@ echo "Deploying ${GITHUB_SHA} to GitHub Pages"
 echo "Initialising in ${PWD} as ${GITHUB_ACTOR}"
 
 git init
+git config set advice.defaultBranchName false
 git config user.name "${GITHUB_ACTOR}"
 git config user.email "${GITHUB_ACTOR}@users.noreply.github.com"
 git remote add origin "https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
